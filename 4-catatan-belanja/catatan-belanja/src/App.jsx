cdca0929b222c0fd1c270aad4dc1aef98255572d
sahyudi <sahyudi.dev@gmail.com>
2024-02-05 16:53:37 +0700

    update module apliaksi catatan

---
+++ 4-catatan-belanja/catatan-belanja/src/App.jsx
@@ -0,0 +1,93 @@
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <GroceryList />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+function Form() {
+  const quantityNum = [...Array(20)].map((_, i) => (
+    <option key={i + 1} value={i + 1}>
+      {i + 1}
+    </option>
+  ));
+  return (
+    <form className="add-form">
+      <div>
+        <select>{quantityNum}</select>
+        <input type="text" placeholder="nama barang..." />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function GroceryList() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id} />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" />
+      <span style={item.checked ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button>&times;</button>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="stats">
+      Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+    </footer>
+  );
+}
